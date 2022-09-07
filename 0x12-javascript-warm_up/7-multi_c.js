cc396dea3023b0ff7a5f0345e19c48a869705810
oriafo <abrahamibharunujele@gmail.com>
2022-09-07 09:43:33 -0700

    I love C

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const argv = process.argv;
+
+const convertedVar = Math.floor(argv[2]);
+
+if (isNaN(convertedVar)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < convertedVar; i++) {
+    console.log('C is fun');
+  }
+
+}
