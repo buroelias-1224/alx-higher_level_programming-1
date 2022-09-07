e4100727bce98dd16eb5f7492129586a672a8afb
oriafo <abrahamibharunujele@gmail.com>
2022-09-07 04:49:10 -0700

    An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,10 +2,11 @@
 
 const argv = process.argv;
 
-const entry = Math.floor(argv[2])
+const entry = Math.floor(argv[2]);
 
 if (isNaN(entry)) {
 	console.log('Not a number');
 } else {
 	console.log(`My number: ${entry}`);
 }
+
